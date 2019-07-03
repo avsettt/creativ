55f65664a94048d2e29c827566ce20d4a3eba7d9
Avset <avsettt@mail.ru>
2019-07-03 15:29:38 +0400

    list2

---
+++ creativ/main.js
@@ -0,0 +1,12 @@
+var icon = document.getElementById("my_icon");
+var jsMenu = document.getElementById("id_menu");
+
+
+icon.addEventListener("click", cntrol)
+function cntrol(){
+if(jsMenu.className === "menu"){
+       jsMenu.className += " jsFix";
+      }else{
+        jsMenu.className = "menu";
+    }
+}
